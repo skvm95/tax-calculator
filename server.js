224b8e0cc424d6f14037d760bdfc384abe45076b
Sandeep Mishra <skvm95@gmail.com>
2025-02-02 01:10:53 +0530

    Adding calculateTax API to get tax as per the Budget 2025 on 1 Feb, 25

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const taxSlabs = [
+  { limit: 400000, rate: 0 },
+  { limit: 800000, rate: 0.05 },
+  { limit: 1200000, rate: 0.10 },
+  { limit: 1600000, rate: 0.15 },
+  { limit: 2000000, rate: 0.20 },
+  { limit: 2400000, rate: 0.25 },
+  { limit: Infinity, rate: 0.30 },
+];
+
+// Function to calculate tax
+const calculateTax = (income) => {
+  //console.log("--- CTC : ", income)
+  let tax = 0;
+  let previousLimit = 0;
+
+  for (let slab of taxSlabs) {
+    if (income > slab.limit) {
+      tax += parseInt((slab.limit - previousLimit) * slab.rate);
+      previousLimit = slab.limit;
+      //console.log(`${slab.limit} has tax ${tax}`)
+    } else {
+      tax += parseInt((income - previousLimit) * slab.rate);
+      //console.log(`Final ${slab.limit} has tax ${tax}`)
+      break;
+    }
+  }
+  return parseInt(tax);
+};
+
+// API endpoint using GET method and query params
+app.get("/calculateTax", (req, res) => {
+  const income = parseFloat(req.query.income);
+  let basicIncome = parseFloat(req.query.basic);
+  const mealCardOpted = req.query.mealCardOpted === "false" ? false : true;
+
+  if (!income || isNaN(income) || income < 0) {
+    return res.status(400).json({ error: "Invalid income amount" });
+  }
+
+  if (!basicIncome || isNaN(basicIncome) || basicIncome < 0) {
+    basicIncome = parseInt((income * 0.37) / 12)
+  }
+
+  console.log("CTC : ", income)
+  console.log("Basic Sal : ", basicIncome)
+
+  const gratutity = parseInt(basicIncome / 26 * 15)
+  console.log("gratutity : ", gratutity)
+
+  const epfo = parseInt(1800 * 2 * 12)
+  console.log("epfo : ", epfo)
+
+  const professionalTax = 2500
+
+  let finalIncome = parseInt(income - gratutity - epfo - professionalTax)
+
+  finalIncome = mealCardOpted ? parseInt(finalIncome - 2200*12) : finalIncome
+  console.log("finalIncome : ", finalIncome)
+
+  const rawTaxPayable = calculateTax(finalIncome);
+  console.log("rawTaxPayable : ", rawTaxPayable)
+
+  const taxPayable = parseInt(rawTaxPayable * 1.04)
+  console.log("taxPayable : ", taxPayable)
+
+  let inhandSalary = parseInt(((finalIncome - taxPayable) / 13))
+  console.log("inhandSal : ", inhandSalary)
+
+  res.json({ income, taxPayable , inhandSalary })
+});
+
+app.get("/health", (req, res) => {
+  const health = "ok"
+  res.json({health})
+})
+
+// Start server
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
